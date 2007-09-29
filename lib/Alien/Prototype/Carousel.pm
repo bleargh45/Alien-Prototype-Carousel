8f9d30fb22ba196660797dfb9b8f5417b268d442
Graham TerMarsch <graham@howlingfrog.com>
2007-09-29 00:48:43 +0000

    Added POD docs, new "--quiet" command line option, and updated version numbering scheme (version=<js-version>.<alien-release>)

--- lib/Alien/Prototype/Carousel.pm
+++ lib/Alien/Prototype/Carousel.pm
@@ -1,28 +1,50 @@
 package Alien::Prototype::Carousel;
 
+###############################################################################
 # Required inclusions.
+###############################################################################
 use strict;
 use warnings;
 use Carp;
 use File::Copy qw(copy);
 use File::Path qw(mkpath);
 
+###############################################################################
 # Version number
-our $VERSION = '0.26';
+###############################################################################
+our $CAROUSEL_VERSION = '0.26';
+our $VERSION = '0.26.1';
 
+###############################################################################
+# Subroutine:   version()
+###############################################################################
 # Return the Prototype Carousel component version number.
+#
+# Not to be confused with the 'Alien::Prototype::Carousel' version number
+# (which is the version number of the Perl wrapper).
+###############################################################################
 sub version {
-    return $VERSION;
+    return $CAROUSEL_VERSION;
 }
 
+###############################################################################
+# Subroutine:   path()
+###############################################################################
 # Returns the path to the available copy of the Prototype Carousel component.
+###############################################################################
 sub path {
     my $base = $INC{'Alien/Prototype/Carousel.pm'};
     $base =~ s{\.pm}{};
     return $base;
 }
 
-# Installs the Prototype Carousel component into the given '$destdir'.
+###############################################################################
+# Subroutine:   install($destdir)
+# Parameters:   $destdir    - Destination directory.
+###############################################################################
+# Installs the Prototype Carousel component into the given '$destdir'.  Throws
+# a fatal exception on errors.
+###############################################################################
 sub install {
     my ($class, $destdir) = @_;
     if (!-d $destdir) {
@@ -55,6 +77,28 @@ Alien::Prototype::Carousel - installing and finding Prototype Carousel component
 =head1 DESCRIPTION
 
 Please see L<Alien> for the manifesto of the Alien namespace.
+
+=head1 METHODS
+
+=over
+
+=item version()
+
+Return the Prototype Carousel component version number. 
+
+Not to be confused with the C<Alien::Prototype::Carousel> version number
+(which is the version number of the Perl wrapper). 
+
+=item path()
+
+Returns the path to the available copy of the Prototype Carousel component. 
+
+=item install($destdir)
+
+Installs the Prototype Carousel component into the given C<$destdir>.
+Throws a fatal exception on errors. 
+
+=back
 
 =head1 AUTHOR
 
